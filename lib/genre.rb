80ea5faa2d4c307ab6bf791f1afbe6b2487ca2fa
Brittany Morris <brittmmendez@gmail.com>
2017-12-19 01:37:37 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,13 +1,22 @@
 class Genre
-attr_accessor :name
+  attr_accessor :name
 
-def initialize(name)
-  @name=name
-  @songs=[]
-end
+  def initialize(name)
+    @name=name
+    @songs=[]
+  end
 
-def add_song(song)
-  @songs<<song
-end
+  def add_song(song)
+    @songs<<song
+  end
 
+  def songs
+    @songs
+  end
+
+  def artist
+    self.songs.collect do |song
+    song.genre
+  end
+  end
 end
